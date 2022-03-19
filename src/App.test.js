72c7cff579fbb7482cd96cdbdf79a8764476d5c7
Michael C <mikkdc@yahoo.co.uk>
2022-03-19 21:22:38 +0000

    added RTL tests to App.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import TodoInput from './components/TodoInput';
 
-test('renders learn react link', () => {
+test('renders title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/to do list/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("Input box is shown to user", () => {
+  render(<App />);
+  const linkElement = screen.getByPlaceholderText(/Please enter a task/i);
+  expect(linkElement).toBeInTheDocument();
+})
+
+test("fireEvent on Submit button", () => {
+  render(<App />)
+  const submitbutton = screen.getByText("Add Task")
+  fireEvent.click(submitbutton)
+  const deleteButton = screen.getByText("X")
+  expect(deleteButton).toBeInTheDocument()
+});
+
